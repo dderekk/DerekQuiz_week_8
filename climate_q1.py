134283959b6bc1f8f00c7241a2060717ca88d751
Derek <Qiujjjj@gmail.com>
2023-09-14 16:09:17 +1000

    complete task 1

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,28 @@
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
 
+# First connect to database/SQLite, and get the value
+SQL = sqlite3.connect('climate.db')
+curv = SQL.cursor()
+curv.execute("SELECT * FROM ClimateData")
+allRows = curv.fetchall()
+SQL.close()
+
+# append the list with the data
+years = [eachRow[0] for eachRow in allRows]
+co2 = [eachRow[1] for eachRow in allRows]
+temp = [eachRow[2] for eachRow in allRows]
+
+# Plotting the data
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_1.png")
